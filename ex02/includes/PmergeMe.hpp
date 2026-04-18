c945e8c9bb6b31cd9b20f29708b9a370b1ce832e
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-04-17 21:13:13 -0300

    fix: ex02 pmergeme

--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -1,5 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 18:33:34 by mhidani           #+#    #+#             */
+/*   Updated: 2026/04/17 19:17:44 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #pragma once
 
 class PmergeMe {
+private:
+
+public:
+	PmergeMe(void);
+	PmergeMe(const PmergeMe& other);
+	PmergeMe& operator=(const PmergeMe& other);
+	virtual ~PmergeMe(void);
 };
--- ex02/src/PmergeMe.cpp
+++ ex02/src/PmergeMe.cpp
@@ -1,2 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 18:33:41 by mhidani           #+#    #+#             */
+/*   Updated: 2026/04/17 19:20:39 by mhidani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(void) {
+}
+
+PmergeMe::PmergeMe(const PmergeMe& other) {
+	*this = other;
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
+	(void)other;
+	return *this;
+}
+
+PmergeMe::~PmergeMe(void) {
+}
